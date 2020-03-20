08dff665c8a33de9ec8cf6488c019f0080802773
Robert Van Haecke <robert.van.haecke@gmail.com>
2020-03-20 10:19:52 +0100

    Add test grpc client and service

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using System;
+using static GrpcService.Greeter;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Lucedy";
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GreeterClient(channel);
+
+            var response = client.SayHello(new GrpcService.HelloRequest { Name = name });
+
+            Console.WriteLine($"{response.Message}!");
+            Console.ReadLine();
+        }
+    }
+}
